c45c920b54de576dc34ac8da56c48c5d0852d868
QASAK <w663012911@outlook.com>
2022-09-28 17:30:36 +0800

    complete ChunkType

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -1,3 +1,68 @@
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct ChunkType {
+    chunk_type: [u8; 4],
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+    type Error = ();
+
+    fn try_from(value: [u8; 4]) -> Result<Self, Self::Error> {
+        if value.iter().all(|c| c.is_ascii_alphabetic()) {
+            Ok(ChunkType{chunk_type: value})
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl FromStr for ChunkType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() == 4 && s.bytes().into_iter().all(|c| c.is_ascii_alphabetic()) {
+            Ok(ChunkType{chunk_type: <[u8; 4]>::try_from(s.as_bytes()).unwrap() })
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl Display for ChunkType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", String::from_utf8(self.chunk_type.to_vec()).unwrap())
+    }
+}
+
+impl ChunkType {
+    fn bytes(&self) -> [u8; 4] {
+        self.chunk_type
+    }
+
+    fn is_valid(&self) -> bool {
+        self.chunk_type.iter().all(|c| c.is_ascii_alphabetic()) && self.chunk_type[2].is_ascii_uppercase()
+    }
+
+    fn is_critical(&self) -> bool {
+        self.chunk_type[0].is_ascii_uppercase()
+    }
+
+    fn is_public(&self) -> bool {
+        self.chunk_type[1].is_ascii_uppercase()
+    }
+
+    fn is_reserved_bit_valid(&self) -> bool {
+        self.chunk_type[2].is_ascii_uppercase()
+    }
+
+    fn is_safe_to_copy(&self) -> bool {
+        self.chunk_type[3].is_ascii_lowercase()
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
